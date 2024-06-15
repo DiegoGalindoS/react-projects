7a26a10783f99edc9234c6183ecd35b80ef3b8ee
Andres Paez <apaez698@gmail.com>
2024-06-15 13:14:13 -0500

    Hooks 001

--- react-projects/src/App.jsx
+++ react-projects/src/App.jsx
@@ -1,12 +1,37 @@
 import { XFollowCard } from "./XFollowCard";
 import "./App.css";
-
+const users = [
+  {
+    username: "PhysicsToday",
+    profilename: "PhysicsToday",
+    isfollowing: true,
+  },
+  {
+    username: "ScienceMagazine",
+    profilename: "Science Magazine",
+    isfollowing: false,
+  },
+  {
+    username: "NASA",
+    profilename: "NASA",
+    isfollowing: true,
+  },
+  {
+    username: "elonmusk",
+    profilename: "Elon Musk",
+    isfollowing: true,
+  },
+];
 export function App() {
   return (
-    <>
-      <XFollowCard username="PhysicsToday" name="Physics Today" />
-      <XFollowCard username="ScienceMagazine" name="Science Magazine" />
-      <XFollowCard username="NASA" name="NASA" />
-    </>
+    <section className="App">
+      {users.map(({ username, profilename, isfollowing }) => (
+        <XFollowCard
+          username={username}
+          name={profilename}
+          initialState={isfollowing}
+        ></XFollowCard>
+      ))}
+    </section>
   );
 }
--- react-projects/src/XFollowCard.jsx
+++ react-projects/src/XFollowCard.jsx
@@ -1,5 +1,17 @@
-export function XFollowCard({ username, name }) {
+import { useState } from "react";
+
+export function XFollowCard({ username, name, initialState }) {
+  const [isFollowing, setIsFollowing] = useState(initialState);
   const img = `https://unavatar.io/twitter/${username}`;
+  const text = isFollowing ? "Siguiendo" : "Seguir";
+  const buttonClassName = isFollowing
+    ? "tw-followcard-button is-following"
+    : "tw-followcard-button";
+
+  const handleClick = () => {
+    setIsFollowing(!isFollowing);
+  };
+
   return (
     <article className="tw-followcard">
       <header className="tw-followcard-header">
@@ -10,7 +22,9 @@ export function XFollowCard({ username, name }) {
         </div>
       </header>
       <aside>
-        <button className="tw-followcard-button">Seguir</button>
+        <button onClick={handleClick} className={buttonClassName}>
+          {text}
+        </button>
       </aside>
     </article>
   );
